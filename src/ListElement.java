7c64a22e9ec3a77f6e0a2c95e067de73b367572c
Mfonisoabasi James <mfonisoabasijames@yahoo.com>
2018-10-16 01:28:24 -0600

    first declarations

--- src/ListElement.java
+++ src/ListElement.java
@@ -11,6 +11,21 @@ public class ListElement {
 	/**
 	 * @param args
 	 */
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data =0;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
